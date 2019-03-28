1fbd50798720843a7e53e408dc424739018901b8
Julio_Leme <julioromao3@gmail.com>
2019-03-27 23:25:58 -0300

    Version 1.3 B2W - Julio Dev challenge

--- src/main/java/br/com/b2w/controller/Controller.java
+++ src/main/java/br/com/b2w/controller/Controller.java
@@ -1,10 +1,17 @@
 package br.com.b2w.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.logging.Logger;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.b2w.consumer.EndpointConsumer;
+import br.com.b2w.utils.HelperUtils;
+import br.com.b2w.vo.ItemVO;
 import br.com.b2w.vo.ItemsVO;
 /**
  * 
@@ -16,12 +23,37 @@ import br.com.b2w.vo.ItemsVO;
 @RestController
 public class Controller {
 
-	private static final Logger LOG = Logger.getLogger(Controller.class.getName());
+	private static final Logger logger = Logger.getLogger(Controller.class.getName());
 	
-	private static ItemsVO items;
-
+	@Autowired
+	EndpointConsumer serviceApp;
+	
 	@RequestMapping("/challenge-backend")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
+	
+	@RequestMapping("/challenge-backend")
+	public ItemsVO findByDescricao(@RequestParam("begindate") String begindate,
+			                       @RequestParam("finaldate") String finaldate) {
+		logger.info("--- solucao challenge-bakend ---");
+		
+		ItemsVO itemsoriginal = this.serviceApp.getItemsByEndpoint();
+		ItemsVO itemsnovo = new ItemsVO();
+		ArrayList<ItemVO> listaitemnovo = new ArrayList<>();
+		
+		Date databegin = HelperUtils.convertStringtoDate(begindate);
+		Date datafinal = HelperUtils.convertStringtoDate(finaldate);
+		
+		for (ItemVO item : itemsoriginal.getItems()) {
+			String dataitem = item.getDate();
+			dataitem = HelperUtils.treatmentDate(dataitem);
+			Date dataitemconvert = HelperUtils.convertStringtoDate(dataitem);
+			if(dataitemconvert.after(databegin) && dataitemconvert.before(datafinal))
+				listaitemnovo.add(item);
+		}
+		itemsnovo.setItems(listaitemnovo);
+		return itemsnovo;
+	}
+	
 }
